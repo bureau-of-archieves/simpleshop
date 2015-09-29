51d26944ffdf97f2846823f2cede7439faec231c
zhy2002 <hz141@uowmail.edu.au>
2015-09-30 00:12:29 +1000

    Bug fix - Order.orderItems always return null.

--- simpleshop-service/src/main/java/simpleshop/service/impl/OrderServiceImpl.java
+++ simpleshop-service/src/main/java/simpleshop/service/impl/OrderServiceImpl.java
@@ -1,12 +1,20 @@
 package simpleshop.service.impl;
 
 import org.springframework.stereotype.Service;
+import simpleshop.domain.model.Order;
 import simpleshop.domain.model.component.OrderItem;
 import simpleshop.service.OrderService;
 import simpleshop.service.impl.base.OrderBaseService;
 
+import javax.validation.constraints.NotNull;
+
 @Service
 public class OrderServiceImpl extends OrderBaseService implements OrderService {
+
+    @Override
+    protected void initialize(@NotNull Order model) {
+        orderDAO.initialize(model.getOrderItems());
+    }
 
     @Override
     public OrderItem createOrderItem() {
